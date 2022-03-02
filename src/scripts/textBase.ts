2159542e3dc049bd6857999d39da76efd94ba234
SashaTsarikova <Sashatsarikova@mail.ru>
2022-03-02 16:06:46 +0300

    feat: create sections till skills

---
+++ src/scripts/textBase.ts
@@ -0,0 +1,33 @@
+const textBaseEng = {
+  town: 'Minsk city',
+  roleS: 'Frontend developer',
+  aboutMe: '',
+  hardSkills: {
+    'IT languages': [
+      'HTML5',
+      'CSS / SCSS',
+      'JS / TS',
+      'SQL / MySQL',
+      'Basic',
+    ],
+    'Other relevant IT skills': [
+      'GIT',
+      'Figma',
+      'Chrome DevTools',
+    ],
+    Lenguages: [
+      'Russian (native)',
+      'Belorussian (native)',
+      'English (B1)',
+      'Spanish (A1)'],
+  },
+  softSkills: {
+    'communication skills': '',
+    'research skills': '',
+    responsibility: '',
+    'emotional intelligence': '',
+    'teamwork ability': '',
+    'critical thinking': '',
+  },
+
+};
